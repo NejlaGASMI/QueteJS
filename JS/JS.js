9e1259ae4b0818c0f1c36b7c13a0f6b1e4ff2de7
NejlaGASMI <44712147+NejlaGASMI@users.noreply.github.com>
2019-02-12 13:44:06 +0100

    Add files via upload

---
+++ JS/JS.js
@@ -0,0 +1,4 @@
+let film = "Avatar" ;
+let dateDeSortie = "16 décembre 2009" ;
+let realisateur = "James Cameron" ;
+alert(`Mon film préféré est ${film}, il est sorti le ${dateDeSortie} et a été réalisé par ${realisateur}.`)
